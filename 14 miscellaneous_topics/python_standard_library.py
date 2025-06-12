029ed7264ad843df1fbd4d701344845794ba0b22
Swappy514 <swaroopjadhav5@gmail.com>
2025-06-12 09:48:00 +0530

    Add python_standard_library.py

---
+++ 14 miscellaneous_topics/python_standard_library.py
@@ -0,0 +1,16 @@
+# Python Standard Library Examples
+
+import math
+import datetime
+import random
+
+# Example 1: Using math module
+print("Square root of 16 is", math.sqrt(16))
+print("Value of pi is", math.pi)
+
+# Example 2: Using datetime module
+now = datetime.datetime.now()
+print("Current date and time:", now)
+
+# Example 3: Using random module
+print("Random number between 1 and 10:", random.randint(1, 10))
