39770441e9d2d4925415c492ea5b426f0b641d09
Swappy514 <swaroopjadhav5@gmail.com>
2025-06-20 09:18:00 +0530

    Add conditional_statements.py

---
+++ 3 operators_and_conditional_statement/conditional_statements.py
@@ -0,0 +1,26 @@
+# Conditional Statements in Python
+
+age = 18
+
+# Example 1: if statement
+if age >= 18:
+    print("You are an adult.")
+
+# Example 2: if-else statement
+marks = 75
+if marks >= 50:
+    print("You passed!")
+else:
+    print("You failed!")
+
+# Example 3: if-elif-else ladder
+score = 85
+
+if score >= 90:
+    print("Grade: A")
+elif score >= 75:
+    print("Grade: B")
+elif score >= 50:
+    print("Grade: C")
+else:
+    print("Fail")
