636bea39116a6dc8cc0242846021cc79ac29d75e
Swappy514 <swaroopjadhav5@gmail.com>
2025-06-22 09:51:00 +0530

    Add relational_operators.py

---
+++ 3 operators_and_conditional_statement/relational_operators.py
@@ -0,0 +1,14 @@
+# Relational Operators in Python
+# Used to compare values, result is always a boolean (True or False)
+
+# Examples:
+
+a = 10
+b = 20
+
+print("a == b:", a == b)  # Equal to? False
+print("a != b:", a != b)  # Not equal? True
+print("a > b:", a > b)    # Greater than? False
+print("a < b:", a < b)    # Less than? True
+print("a >= 10:", a >= 10)  # Greater or equal? True
+print("b <= 20:", b <= 20)  # Less or equal? True
