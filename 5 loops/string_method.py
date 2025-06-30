d21406a7201f4594f42149276c7b1b204e8bbb91
Swappy514 <swaroopjadhav5@gmail.com>
2025-06-30 14:29:00 +0530

    Add string_method.py

---
+++ 5 loops/string_method.py
@@ -0,0 +1,8 @@
+# String Methods in Loop Context
+
+sentence = "python programming"
+
+for char in sentence:
+    # Print uppercase of each character
+    print(char.upper(), end="")
+print()  # newline after loop
