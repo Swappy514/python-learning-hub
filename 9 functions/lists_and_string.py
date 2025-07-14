b462c5c09a16c694a1b8253e9838aebb92999887
Swappy514 <swaroopjadhav5@gmail.com>
2025-07-14 18:40:00 +0530

    Add lists_and_string.py

---
+++ 9 functions/lists_and_string.py
@@ -0,0 +1,15 @@
+# Lists and Strings in Python: Basic usage and interaction
+
+# Example 1: Iterate over a string and a list
+text = "hello"
+chars = ['h', 'e', 'l', 'l', 'o']
+
+# Print each character from string
+for char in text:
+    print(char, end=' ')
+print()
+
+# Print each element from list
+for c in chars:
+    print(c, end=' ')
+print()
